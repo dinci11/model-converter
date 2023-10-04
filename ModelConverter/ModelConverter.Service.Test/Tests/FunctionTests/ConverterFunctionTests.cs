9fdec65d383dc989271174c2fbd21f9b6bf93979
Denes Elo <denes.elo@betssongroup.com>
2023-10-04 21:02:51 +0200

    Add function test

---
+++ ModelConverter/ModelConverter.Service.Test/Tests/FunctionTests/ConverterFunctionTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ModelConverter.Common.DTOs.Requestes;
+using ModelConverter.Common.DTOs.Responses;
+using ModelConverter.Common.Services;
+using ModelConverter.Common.Services.Interfaces;
+using ModelConverter.Service.Functions;
+using ModelConverter.Service.Services.Interfaces;
+using ModelConverter.Service.Test.Tests.Helpers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelConverter.Service.Test.Tests.FunctionTests
+{
+    public class ConverterFunctionTests
+    {
+        private ConverterFunction function;
+        private IConverterService converterServiceMock;
+        private ILogger<ConverterFunction> loggerMock;
+        private IValidator<ModelConvertingRequest> validatorMock;
+        private IExceptionHandler exceptionHandlerMock;
+        private HttpRequestMockProvider requestMockProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            converterServiceMock = Mock.Of<IConverterService>();
+            loggerMock = Mock.Of<ILogger<ConverterFunction>>();
+            validatorMock = Mock.Of<IValidator<ModelConvertingRequest>>();
+            exceptionHandlerMock = new ExceptionHandler(Mock.Of<ILogger<ExceptionHandler>>());
+            requestMockProvider = new HttpRequestMockProvider();
+            function = new ConverterFunction(converterServiceMock, loggerMock, validatorMock, exceptionHandlerMock);
+        }
+
+        [Test]
+        public async Task RunFunction_StartConvertingSuccessfuly_ReturnOkobjectResult()
+        {
+            //Arrange
+            var requestBody = new ModelConvertingRequest()
+            {
+                ProcessId = "someId",
+                InputPath = "c:",
+                OutputPath = "c:",
+                TargetFormat = Common.Enums.TargetFormat.Obj
+            };
+
+            requestMockProvider.SetRequestBody(requestBody);
+
+            var request = requestMockProvider.httpRequest;
+
+            //Act
+            var response = await function.Run(request);
+
+            //Assert
+            Assert.IsInstanceOf<OkObjectResult>(response);
+            var okObjectResult = (OkObjectResult)response;
+            Assert.IsNotNull(okObjectResult);
+            var convertingResponse = okObjectResult.Value as ModelConvertingResponse;
+            Assert.AreEqual(requestBody.ProcessId, convertingResponse.ProcessId);
+        }
+    }
+}
---
+++ ModelConverter/ModelConverter.Service.Test/Tests/Helpers/HttpRequestMockProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ModelConverter.Service.Test.Tests.Helpers
+{
+    public class HttpRequestMockProvider
+    {
+        public HttpRequest httpRequest;
+
+        public HttpRequestMockProvider()
+        {
+            httpRequest = Mock.Of<HttpRequest>();
+        }
+
+        public void SetRequestBody(object body)
+        {
+            var jsonString = JsonSerializer.Serialize(body);
+            var requestBodyStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+
+            httpRequest.Body = requestBodyStream;
+        }
+    }
+}
